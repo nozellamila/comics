094aa6ad27e5ed1c7a2b04c65c1b017f3cd95ad4
Mila Figueira Nozella <base2114@localiza.com>
2021-07-03 11:11:21 -0300

    Primeiro commit

---
+++ src/test/java/com/gerenciador/comics/ComicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.comics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
